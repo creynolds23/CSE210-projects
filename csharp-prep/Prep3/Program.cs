8087ae378411edcd0dec9fd4fc2f4688fd723224
Conner Reynolds <rey18008@byui.edu>
2023-01-09 10:07:27 -0700

    Prep 3 loop finish

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        //Console.WriteLine($"Your random number is {number}");
+        
+        bool guessed = false;
+        int turns = 0;
+        while (guessed == false)
+        {
+            Console.Write("Please enter your guess: ");
+            int answer = int.Parse(Console.ReadLine());
+            turns += 1;
+            if (answer > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (answer < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Correct!");
+                guessed = true;
+            }
+        }
+        Console.WriteLine($"It took you {turns} turns to guess the number");
+        //Console.WriteLine($"They guessed {answer}");
     }
 }
