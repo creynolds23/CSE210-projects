003637a7f0288fc73a87048a05330c1e822c01da
Conner Reynolds <rey18008@byui.edu>
2023-02-14 19:54:00 -0700

    Learning Week 4 Finished

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignments assignment2 = new MathAssignments("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignments assignment3 = new WritingAssignments("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
+        Console.WriteLine();
+
+
     }
 }
